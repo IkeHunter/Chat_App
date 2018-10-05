055cf5cdbb1d25297bebf1e0bd6b1c84e6cd1e80
Isaac <ikehunter5@gmail.com>
2018-10-05 05:36:47 -0400

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'John',
-    text: 'wassup'
-  });
 });
 
 socket.on('disconnect', function () {
